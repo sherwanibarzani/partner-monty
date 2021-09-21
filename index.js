c735defefa05d06193e180ad395c926d7f1f4f84
sherwanibarzani <91080368+sherwanibarzani@users.noreply.github.com>
2021-09-21 13:24:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ client.on('message', message => {
 
 if (!message.guild) return; 
 
-if (message.content === 'wara') { 
+if (message.content === 'oliver') { 
  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 if (message.member.voiceChannel) { 
 
-message.member.voiceChannel.join() 
+message.member.voiceChannel.join(824010186712809520) 
 
-.then(connection => { 
+.then(connection => { +oliver
 
 
 }) 
@@ -21,7 +21,7 @@ message.member.voiceChannel.join()
 
 } else { 
 
-message.reply('**<a:9FAF7C0CF0104A2F979F1263DF55D91A:855230044258828328> پێویستە تۆ لە ڤۆیسەکە بیت **'); 
+message.reply('** پێویستە تۆ لە ڤۆیسەکە بیت **'); 
 
 } 
 
@@ -36,10 +36,10 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/AkYA68Z2gG`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/YUwXsZV2T3`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("841792552314994709").send(
-`> <a:E14567BBEE164342AF58DF6872011807:853287588390633502> By: <@${message.author.id}> ✅ 
+  client.channels.get("824010185315713063").send(
+`>  By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
     }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Rose 💙`) 
+client.user.setGame(`oliver 💙`) 
 });
 
 
 
 
-client.login("");// Your Token
+client.login("ODg5ODMwMTUyODkxMTU0NDMy.YUm86A.EXFYzjzJ6WfN0k-2nhM2lNQ7-TI");// Your Token
